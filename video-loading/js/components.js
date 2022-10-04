2f8a38a860d1bdb948b0ac0229fb708df48449c9
CodyMoltar <66773011+CodyMoltar@users.noreply.github.com>
2022-10-04 16:42:36 +0300

    added more videos for tests

--- video-loading/js/components.js
+++ video-loading/js/components.js
@@ -1,8 +1,10 @@
 let cameraPosition;
+let videos;
 
 AFRAME.registerComponent('positionlogger', {
     init: function(){
         this.cam = document.getElementById('rig');
+        videos = document.getElementsByTagName('a-video');
     },
     tick: function(){
         cameraPosition = this.cam.object3D.getWorldPosition(new THREE.Vector3())
@@ -11,80 +13,61 @@ AFRAME.registerComponent('positionlogger', {
 
 AFRAME.registerComponent('videoloop', {
     schema: {type: 'int', default: 5},
-    init: function () {
+    init: function (){
         // Set the static image source of the plane
         this.staticImageURL = './media/' + this.el.id + ".png"
         this.el.setAttribute('src', this.staticImageURL);
+        this.el.setAttribute('rotation', '20,0,0')
         this.videoLoopURL = './media/' + this.el.id + "-loop.mp4"
-
-        this.triggerDistance = 40;
-
+        this.triggerDistance = 20;
         this.loopLoaded = false;
 
     },
     tick: function(){
-
         if(!this.loopLoaded){
             this.videoPosition = this.el.getObject3D("mesh").getWorldPosition(new THREE.Vector3());
-        
             this.distance = this.videoPosition.distanceTo(cameraPosition)
-
             if(this.distance < this.triggerDistance){
-            
                 console.log("Loading loop");
-
                 this.el.setAttribute('src', this.videoLoopURL)
-
                 this.loopLoaded = true;
-
             }
         }
-
-        
-
     }
 });
 
 AFRAME.registerComponent('videoactivator', {
     schema:{
-        color: {default: '#808080'},
+        color: {default: '#fff'},
         target: {type: 'string', default: 'none'}
     },
     init: function(){
-
-        this.width = 2;
-        this.height = 2;
-
-        this.el.setAttribute('color', this.data.color);
+        this.width = 4;
+        this.height = 4;
+        // this.el.setAttribute('color', this.data.color);
+        this.el.setAttribute(
+            'material', {
+                src: './media/feet.png',
+                transparent: true
+            })
         this.el.setAttribute('width', this.width)
         this.el.setAttribute('height', this.height)
-
-        this.videos = document.getElementsByTagName('a-video');
         this.videoURL = './media/' + this.data.target + ".mp4"
         this.videoLoaded = false;
 
     },
     tick: function(){
-
         if(!this.videoLoaded){
-
             this.planePosition = this.el.getObject3D("mesh").getWorldPosition(new THREE.Vector3());
             this.distance = this.planePosition.distanceTo(cameraPosition)
-    
             if(this.distance < this.width && this.distance < this.height){
-                for(let i = 0; i < this.videos.length; i++){
-                    if(this.videos[i].id === this.data.target){
-                        this.videos[i].setAttribute('src', this.videoURL);
+                for(let i = 0; i < videos.length; i++){
+                    if(videos[i].id === this.data.target){
+                        videos[i].setAttribute('src', this.videoURL);
                     }
                 }
-
                 this.videoLoaded = true;
-    
             }
-
         }
-
-        
-
     }
 })
