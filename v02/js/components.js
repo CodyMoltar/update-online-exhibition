7e48f5c3f1fa429e09576a4e303b8303611064e2
CodyMoltar <66773011+CodyMoltar@users.noreply.github.com>
2022-11-08 20:42:23 +0200

    continue work

--- v02/js/components.js
+++ v02/js/components.js
@@ -22,12 +22,12 @@ AFRAME.registerComponent('show', {
 AFRAME.registerComponent('positionlogger', {
     init: function(){
         this.cam = document.getElementById('cameraRig');
-        videos = document.getElementsByTagName('a-video');
     },
     tick: function(){
         cameraPosition = this.cam.object3D.getWorldPosition(new THREE.Vector3())
     }
 })
+
 
 AFRAME.registerComponent('2dvideoplayer', {
     schema:{
@@ -37,32 +37,20 @@ AFRAME.registerComponent('2dvideoplayer', {
         videoheight: {default: 4.5},
         planewidth: {default:2},
         planeheight: {default:2},
-        triggerdistance: {default: 20}
+        triggerdistance: {default: 20},
+        lookangle: {default: 0}, 
+        lookposition: {type: 'vec3'}
     },
     init: function(){
 
         // add the video as a video element to control playback
         this.videoSource = document.createElement('video');
+        this.videoSource.muted = true;
+        this.videoSource.autoplay = true;
         this.videoSource.id = this.data.target;
         this.videoSource.src = './media/videos/' + this.data.target + '.mp4';
         // this.video.onloadeddata = this.onLoaded.bind(this);
         document.body.appendChild(this.videoSource);
-
-        // Create the frame entity
-        this.frame = document.createElement('a-entity');
-        this.el.appendChild(this.frame);
-        this.frame.setAttribute('scale', { x: 0.090, y: 0.115, z: 0.100});
-        this.frame.setAttribute('rotation', { x: 20, y: 0, z: -90 });
-        this.frame.setAttribute('position', { x: -4.359, y: 3, z: -0.07 });
-        this.frame.setAttribute('gltf-model', 'url(./media/3dmodels/frame.glb)')
-        // this.frame.setAttribute('show', '');
-
-        // this.frame = document.createElement('a-obj-model');
-        // this.el.appendChild(this.frame);
-        // this.frame.setAttribute('scale', { x: 0.090, y: 0.115, z: 0.100});
-        // this.frame.setAttribute('rotation', { x: 20, y: 0, z: -90 });
-        // this.frame.setAttribute('position', { x: -4.359, y: 3, z: -0.07 });
-        // this.frame.setAttribute('src', './media/3dmodels/frame.obj')
 
         // Create the video entity
         this.video = document.createElement('a-video');
@@ -73,6 +61,24 @@ AFRAME.registerComponent('2dvideoplayer', {
         this.video.setAttribute('height', this.data.videoheight);
         this.video.setAttribute('position', { x: 0, y: 3, z: 0 });
         this.video.setAttribute('rotation', '20,0,0');
+
+        // Create the frame entity
+        this.frame = document.createElement('a-box');
+        this.frame.setAttribute('scale', { x: 1, y: 1, z: 1});
+        this.frame.setAttribute('rotation', { x: 0, y: 0, z: 0 });
+        this.frame.setAttribute('position', { x: -0, y: 0, z: -0.3});
+        this.frame.setAttribute('width', this.data.videowidth + 0.5);
+        this.frame.setAttribute('height', this.data.videoheight + 0.3);
+        this.frame.setAttribute('color', '#6e6256');
+        this.frame.setAttribute('depth', 0.5);
+        this.frame.classList.add('video');
+        this.video.appendChild(this.frame);
+
+        this.frame.setAttribute('enable-interaction', {
+            lookangle: this.data.lookangle,
+            lookposition: this.data.lookposition.x + ' ' + this.data.lookposition.y + ' ' + this.data.lookposition.z
+        })
+
 
         // Create the plane entity
         this.plane = document.createElement('a-plane');
@@ -87,38 +93,37 @@ AFRAME.registerComponent('2dvideoplayer', {
         this.plane.setAttribute('height', '2');
         this.plane.setAttribute('position', { x: 0, y: 0.01, z: 5 });
         this.plane.setAttribute('rotation', '-90,0,0');
-        this.plane.s
-
-        this.videoloaded = false;
-        this.looploaded = false;
 
         this.videoURL = './media/videos/' + this.data.target + '.mp4'
         this.loopURL = './media/videos/' + this.data.target + '-loop.mp4'
 
-        this.getWorldPos = false;
+        this.videoloaded = false;
+        this.looploaded = false;
+
+        this.ringAdded = false;
 
     },
     tick: function(){
+
+        
 
         if(!this.videoloaded){
 
             this.planePosition = this.plane.getObject3D("mesh").getWorldPosition(new THREE.Vector3());
             this.distance = this.planePosition.distanceTo(cameraPosition);
 
-            // if(!this.looploaded){
-            //     if(this.distance < this.data.triggerdistance && this.distance < this.data.triggerdistance){
-            //         console.log('loading loop..');
-            //         this.video.setAttribute('src', this.loopURL);
-            //         this.looploaded = true;
-            //     }
-            // }
-            // else if(this.distance < this.data.planewidth && this.distance < this.data.planeheight){
-            //     console.log('loading video...');
-            //     this.video.setAttribute('src', this.videoURL);
-            //     this.videoloaded = true;
-            // }
+            if(!this.ringAdded){
 
-            
+                this.frame.setAttribute('ring', {
+                    color:'#fff',
+                    radius: 0.5,
+                    width: 0.2,
+                    position: this.planePosition.x + ' 0.05 ' + this.planePosition.z
+                })
+                
+                this.ringAdded = true;
+            }
+
             if(this.distance < this.data.planewidth && this.distance < this.data.planeheight){
                 console.log('loading video...');
                 this.video.setAttribute('material', 'src', '#' + this.data.target);
@@ -182,12 +187,12 @@ AFRAME.registerComponent('360videoplayer', {
             }
         })
 
-        this.getWorldPos = false;
+        this.ringAndLookinteractionAdded = false;
         
     },
     tick: function(){
 
-        if(!this.getWorldPos){
+        if(!this.ringAndLookinteractionAdded){
             this.worldPosition = this.cylinder.getObject3D("mesh").getWorldPosition(new THREE.Vector3());
             // console.log(this.worldPosition);
             this.videoSphere.setAttribute('ring', {
@@ -200,7 +205,7 @@ AFRAME.registerComponent('360videoplayer', {
                 lookangle: 180,
                 lookposition: this.worldPosition.x + ' ' + this.worldPosition.y + ' ' + this.worldPosition.z
             })
-            this.getWorldPos = true;
+            this.ringAndLookinteractionAdded = true;
         }
 
     }
